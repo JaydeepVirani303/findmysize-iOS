db689b63e9714f22a10807c92889763684d662ca
Jaydeep <viranijaydeep303@gmail.com>
2021-04-18 21:11:35 +0530

    Create example

--- Example/Pods/Target Support Files/Pods-findmysize_Example/Pods-findmysize_Example-frameworks.sh
+++ Example/Pods/Target Support Files/Pods-findmysize_Example/Pods-findmysize_Example-frameworks.sh
@@ -175,9 +175,19 @@ code_sign_if_enabled() {
 }
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
+  install_framework "${BUILT_PRODUCTS_DIR}/Alamofire/Alamofire.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/AlamofireSwiftyJSON/AlamofireSwiftyJSON.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/ObjectMapper/ObjectMapper.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/ReachabilitySwift/Reachability.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/SwiftyJSON/SwiftyJSON.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/findmysize/findmysize.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
+  install_framework "${BUILT_PRODUCTS_DIR}/Alamofire/Alamofire.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/AlamofireSwiftyJSON/AlamofireSwiftyJSON.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/ObjectMapper/ObjectMapper.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/ReachabilitySwift/Reachability.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/SwiftyJSON/SwiftyJSON.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/findmysize/findmysize.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
--- Example/findmysize/ViewController.swift
+++ Example/findmysize/ViewController.swift
@@ -7,8 +7,19 @@
 //
 
 import UIKit
+import findmysize
 
 class ViewController: UIViewController {
+    
+    //MARK:- Outlet Zone
+    
+    @IBOutlet weak var btnMySize:UIButton!
+       
+    //MARK:- Outlet Zone
+    
+    let obj = FindMySize()
+    
+    //MARK:- View Life Cycle
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,6 +29,20 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    //MARK:- Action Zone
+    
+    @IBAction func btnFindMyFitAction(_ sender: UIButton) {
+        let strCurrentSize = obj.getSizeByAttribute(userId: "1", attributeName: "Outer Wear-FREE")
+        if strCurrentSize.isEmpty {
+            obj.openSizePopup(userId: "1", attributeName: "Dresses",controller: self)
+            obj.handlerUpdate = {[weak self] size in
+                self?.btnMySize.setTitle("Your size is \(size)", for: .normal)
+            }
+        } else {
+            btnMySize.setTitle("Your size is \(strCurrentSize)", for: .normal)
+        }
     }
 
 }
